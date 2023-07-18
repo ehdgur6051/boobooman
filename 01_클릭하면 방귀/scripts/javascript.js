acac7168637f484563ce255243e48cce767532f0
DH Lee <ehdgur6051@gmail.com>
2023-07-18 18:12:58 +0900

    5th commit

--- 01_클릭하면 방귀/scripts/javascript.js
+++ 01_클릭하면 방귀/scripts/javascript.js
@@ -3,16 +3,16 @@ var clicks = 0;
 var startCount = 0;
 var countDisplayed = false; // 카운트 다운 시작됐는지 확인용
 var countdownComplete = false; // 남은시간 0 됐을 때 확인용
-var oneSec = 10
 
 //exp 설정 : n번 누르면 레벨 업
 var Lv1 = 5
-var Lv2 = 6
-var Lv3 = 7
-var Lv4 = 8
-var Lv5 = 9
-var Lv6 = 10
-var Lvs = [Lv1, Lv2, Lv3, Lv4, Lv5, Lv6]
+var Lv2 = 10
+var Lv3 = 20
+var Lv4 = 40
+var Lv5 = 80
+var Lv6 = 100
+var lastLv = 999
+var Lvs = [Lv1, Lv2, Lv3, Lv4, Lv5, Lv6, lastLv]
 var curLv = 0
 var curExp = Lvs[curLv] ;
 
@@ -31,6 +31,9 @@ var fartE_Imgs = ["images/FX_fart_empty.png", "images/FX_fartE_01.png", "images/
 //변신 이펙트 어레이
 var transformFx_imgs = ["images/FX_A_01.png", "images/FX_A_02.png", "images/FX_A_03.png", "images/FX_A_04.png", "images/FX_A_05.png"]
 
+//넥스트 레벨 이미지 
+var nextLevel_Imgs = ["images/next_level_02.png", "images/next_level_03.png", "images/next_level_04.png", "images/next_level_05.png", "images/next_level_06.png", "images/next_level_07.png", "images/last_level.png"]
+
 //배경음악 경로 어레이, 사이 텀
 var bgmPath = ["sounds/bgm01.wav", "sounds/bgm02.wav", "sounds/bgm03.wav"]
 var curMusicIdx = 0 ; 
@@ -42,18 +45,17 @@ var audioElement = document.createElement("audio");
 $(function(){
     document.getElementById("exp").innerHTML = curExp ; // exp 표시
     document.getElementById('Character').src = CH_01[curLv]; // 캐릭터 이미지 표시
+    document.getElementById('countDown').innerHTML = 30 ; // 남은시간 설정
     document.body.appendChild(audioElement);
     })
 
 function fart(){
-
     //방귀 사운드 재생
     var fartA_sfx = new Audio();
     fartA_sfx.src = "sounds/fart01_sfx.wav";
     fartA_sfx.play();
 
     //방귀 이펙트 재생
-    // Change images with a 0.1-second interval
     var fartA_fx = document.getElementById('fart');
     for (var i = 0; i < fartA_Imgs.length; i++) {
         setTimeout(function(i) {
@@ -62,27 +64,11 @@ function fart(){
     }
 
     //캐릭터 이미지 0.3초 동안 바뀜(방귀뀌는 포즈)
-    if(document.getElementById('countDown').innerHTML < 2){
-        const last1sec = setInterval(() => { //버그 때문에 마지막 0.3초 남기고는 이미지 안바뀌게 설정 함
-            oneSec--;
-            console.log(oneSec)
-            if (oneSec > 5){ 
-                document.getElementById('Character').src = CH_02[curLv];
-                clearInterval(last1sec);
-            }else{
-                console.log("end")
-                document.getElementById('Character').src = "images/gameOver.png";
-                clearInterval(last1sec);
-            }
-        }, 100);
-    }else{
-        console.log('else')
         document.getElementById('Character').src = CH_02[curLv];
         setTimeout(function(){ 
-            document.getElementById('Character').src = CH_01[curLv]; // 다시 원래 이미지로
+            document.getElementById('Character').src = CH_01[curLv] // 다시 원래 이미지로
         }, 300);
-    }
-
+    
     if (!countdownComplete) { //남은시간 아직 있을 때
         clicks += 1;
         document.getElementById("score").innerHTML = clicks;
@@ -93,14 +79,14 @@ function fart(){
 
         //exp 0되면 레벨 업 : exp 수치 변경, 캐릭터 변경, 방귀 이펙트 변경
         if (curExp === 0) {
-            curLv++ ; 
+            curLv++ ;
             console.log("level up") ;
             curExp = Lvs[curLv]
+            document.getElementById("next_level").src = nextLevel_Imgs[curLv] ;
             document.getElementById("exp").innerHTML = curExp ; //다음 레벨 exp 로 변경
-
+            
             //변신 fx 재생
             document.getElementById('transform_fx').style.display = 'block'
-            console.log(document.getElementById('transform_fx').style.display)
             var transformFx = document.getElementById('transform_fx');
             for (var i = 0; i < transformFx_imgs.length; i++) {
                 setTimeout(function(i) {
@@ -110,11 +96,17 @@ function fart(){
                     }
                 }, i * 100, i);
             }
+            if(curLv === 6){
+                console.log("last leve")
+                document.getElementById('exp').style.display = "none"
+            }
             
             // 변신 sfx 재생
             var transform_sfx = new Audio();
             transform_sfx.src = "sounds/transform_sfx.wav";
             transform_sfx.play();
+
+            
 
         }
 
@@ -123,6 +115,7 @@ function fart(){
     // 클릭 시작하면 남은시간 카운트 다운되기 시작. 
     var startCount = 1;
     if (startCount === 1 &&!countDisplayed) { //처음 한번 누르면 카운트 다운 시작 됨. 
+        console.log("countdown starts")
         var curTime = document.getElementById("countDown").innerHTML;
 
         const intervalId = setInterval(() => {
@@ -131,17 +124,22 @@ function fart(){
             if (curTime === 0){ // 남은 시간 0되면 멈춤
                 clearInterval(intervalId); // 카운트 다운 멈춤
                 document.getElementById('Character').onclick = null ;
-                document.getElementById('Character').src = "images/gameOver.png"
-                document.getElementById('myScore').style.display = 'block'
-                document.getElementById('restart').style.display = 'block'
-                document.getElementById('finScore').innerHTML = clicks
-                console.log("game over")
+                setTimeout(function(){ 
+                    document.getElementById('Character').src = "images/gameOver.png"
+                    document.getElementById('myScore').style.display = 'block';
+                    document.getElementById('restart').style.display = 'block';
+                    document.getElementById('next_level').style.display = 'none';
+                    document.getElementById('exp').style.display = 'none' ;
+                    document.getElementById('finScore').innerHTML = clicks ;
+                }, 300);              
+                console.log("time's up") ;
                 countdownComplete = true; 
             }
         }, 1000);
         
         //배경음악 재생
         function playBgm() {
+            audioElement.pause();
             audioElement.src = bgmPath[curMusicIdx];
             audioElement.play();
             audioElement.volume = 0.1 ; 
@@ -159,10 +157,51 @@ function fart(){
             playBgm();
         });
 
+        //'빠르게 클릭!!' 사라지게
+        document.getElementById('clickHere').style.display = "none"
+
+
         countDisplayed = true;
     }
 
 }
 
+function reset(){
+    console.log("reset") ;
+    audioElement.pause();
+    clicks = 0;
+    startCount = 0;
+    countDisplayed = false; 
+    countdownComplete = false; 
+    oneSec = 10
+    curLv = 0
+    curExp = Lvs[curLv] ;
+    document.getElementById("next_level").src = nextLevel_Imgs[curLv] ;
+    document.getElementById("score").innerHTML = clicks; 
+    document.getElementById("exp").innerHTML = curExp ; 
+    document.getElementById('Character').src = CH_01[curLv];
+    document.getElementById('countDown').innerHTML = 30 ; //남은시간
+    document.getElementById('Character').onclick = function(){
+        fart() ;
+    } ;
+    document.getElementById('myScore').style.display = 'none';
+    document.getElementById('restart').style.display = 'none';
+    document.getElementById('next_level').style.display = 'block';
+    document.getElementById('exp').style.display = 'block' ;
+    document.getElementById('clickHere').style.display = 'block'
+}
+
+$(function(){
+    document.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            if(document.getElementById('Character').onclick!==null) {
+                fart() ;
+            }
+        } 
+    }) ;
+});
 
 
+function test(){
+    console.log(curLv)
+}
